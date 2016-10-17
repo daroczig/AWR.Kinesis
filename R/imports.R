2ec853f8819d163b4b1955a9f6b6cd65f04db06e
Gergely Daroczi <gergely.daroczi@card.com>
2016-10-18 00:56:28 +0200

    fix futile.logger imports

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 #'
 #' This is a placeholder for storing import directives, please find more details in the \code{README.md} file.
 #' @docType package
-#' @importFrom futile.logger flog.trace
+#' @importFrom futile.logger flog.trace flog.debug flog.info flog.appender appender.file flog.layout
 #' @importFrom jsonlite fromJSON toJSON base64_dec
 #' @name kineRic-package
 NULL
