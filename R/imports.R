b100ba9879eeb377a01a203123119735d74a0cd0
Gergely Daroczi <gergely.daroczi@card.com>
2016-10-18 01:11:45 +0200

    fix toJSON/unbox namespace issue

--- R/imports.R
+++ R/imports.R
@@ -3,6 +3,6 @@
 #' This is a placeholder for storing import directives, please find more details in the \code{README.md} file.
 #' @docType package
 #' @importFrom futile.logger flog.trace flog.debug flog.info flog.appender appender.file flog.layout
-#' @importFrom jsonlite fromJSON toJSON base64_dec
+#' @importFrom jsonlite fromJSON toJSON base64_dec unbox
 #' @name kineRic-package
 NULL
