16c9c31d1c3070e77d3be60d32a110eb9ca8e590
Gergely Daroczi <gergely.daroczi@card.com>
2016-10-20 23:33:00 +0200

    move jars to java folder as per rJava suggests

--- R/imports.R
+++ R/imports.R
@@ -3,6 +3,11 @@
 #' This is a placeholder for storing import directives, please find more details in the \code{README.md} file.
 #' @docType package
 #' @importFrom futile.logger flog.trace flog.debug flog.info flog.appender appender.file flog.layout
-#' @importFrom jsonlite fromJSON toJSON base64_dec unbox
+#' @importFrom jsonlite fromJSON toJSON base64_dec base64_enc unbox
+#' @importFrom rJava .jpackage .new J .jbyte
 #' @name kineRic-package
 NULL
+
+.onLoad <- function(libname, pkgname) {
+    .jpackage(pkgname, lib.loc = libname)
+}
